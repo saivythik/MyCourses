0c291496018d7c91ee23a6930754c0de7d6c9872
MD. JAHIDUL ISLAM SUJAN <jahid.profile@gmail.com>
2020-10-22 22:39:20 +0600

    Update week6.php

--- Coursera/Web-Applications-for-Everybody/Building-Web-Applications-in-PHP/week6.php
+++ Coursera/Web-Applications-for-Everybody/Building-Web-Applications-in-PHP/week6.php
@@ -1,12 +1,35 @@
 <html>
-<head>
-<title>Guessing Game for Charles Severance</title>
-</head>
-<body>
-<h1>Welcome to my guessing game</h1>
-<p>
-Your guess is too low</p>
-</body>
-</html>
-  
 
+<body>
+	<title> Anamitra Musib 41884c79 </title>
+	<h1> Welcome to my guessing game</h1>
+
+	<?php
+$correctnumber=26; //change this number //
+if(isset($_GET['guess']))
+{
+  if (is_numeric($_GET['guess'])===FALSE)
+ {
+echo "Your guess is not a number";
+}
+else if ($_GET['guess']<$correctnumber)
+ {
+echo "Your guess is too low";
+}
+else if ($_GET['guess']>$correctnumber)
+ {
+echo "Your guess is too high";
+}
+else if ($_GET['guess']==$correctnumber)
+ {
+echo "Congratulations - You are right";
+}
+}
+else
+{
+echo "Missing guess parameter <br> Your guess is too short";
+}
+?>
+</body>
+
+</html>
